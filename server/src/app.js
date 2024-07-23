56ce19a072c796b0dd436641d1df78a5f7b126d6
Thapana Thungkam <eitshien@gmail.com>
2024-07-23 08:43:09 +0700

    app

--- server/src/app.js
+++ server/src/app.js
@@ -1,37 +1,15 @@
 let express = require('express')
-let bodyParser = require('body-parser')
 
-var user_details = require('../json/user_details.json')
+let bodyParser = require('body-parser')
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/status', function (req, res ) {
-    res.send('Hello nodejs server')
-})
+require('./routes')(app);
 
-app.get('/powmath', function (req, res) {
-    const base = 3;
-    const exponent = 2;
-    const result = Math.pow(base, exponent);
-    res.send(`Result of ${base}^${exponent} is: ${result}`);
-});
-
-app.get('/users', function (req, res) {
-    res.send(user_details)
-})
-
-app.get('/users/:user_id', function (req, res) {
-    res.send(`ID : ${req.params.user_id}`)
-})
-
-// create user
-app.post('/user/', function (req, res) {
-    res.send(`Create Account : ${JSON.stringify(req.body/*.username*/)}`)
-})
-
+// Localhost Port
 let port = 8081
 
 app.listen(port, function () {
---
+++ server/src/routes.js
@@ -0,0 +1,18 @@
+const UserController = require('./controllers/userController');
+
+module.exports = (app) => {
+    // create user
+    app.post('/user', UserController.create)
+
+    // edit user by id
+    app.put('/users/:userId', UserController.put)
+
+    // delete user
+    app.delete('/users/:userId', UserController.delete)
+
+    // get user by id
+    app.get('/users/:userId', UserController.show)
+
+    // get all user
+    app.get('/users', UserController.index)
+}
