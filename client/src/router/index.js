8f57860cdc781177f8b1a7a35dd4db170bd9cb48
Thapana Thungkam <eitshien@gmail.com>
2024-08-06 09:58:57 +0700

    vue

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+// Users
+import UserIndex from '@/components/Users/Index'
+import UserCreate from '@/components/Users/CreateUser'
+import UserEdit from '@/components/Users/EditUser'
+import UserShow from '@/components/Users/ShowUser'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      path: '/users',
+      name: 'users',
+      component: UserIndex
+    },
+    {
+      path: '/users/create',
+      name: 'users-create',
+      component: UserCreate
+    },
+    {
+      path: '/users/edit',
+      name: 'users-edit',
+      component: UserEdit
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: UserShow
+    },
   ]
 })
